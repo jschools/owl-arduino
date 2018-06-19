1f3cf55bfb727ebb9462292baed11a8d7d7c31ac
Jonathan Schooler <schooler.jonathan@gmail.com>
2018-06-18 20:44:18 -0700

    Improve comments

--- auto_leveling/auto_leveling.ino
+++ auto_leveling/auto_leveling.ino
@@ -47,26 +47,30 @@ void setup(){
 }
 
 void loop() {
+  // read a sampled input value
   double normalized = readNormalized();
-  LOG("in: ")
-  char str[32];
-  dtostrf(normalized, 7, 5, str);
-  LOG(str)
+  
+  if (DEBUG) {
+    LOG("in: ")
+    char str[16];
+    dtostrf(normalized, 7, 5, str);
+    LOG(str)
+  }
 
   // noise floor threshold
   normalized = thresholdFilter(normalized);
 
-  // adjust scale
+  // update the auto-level logic
   adjustScaleForInput(normalized);
 
-  // scale
+  // scale the input
   normalized *= inputScale;
   LOG("; scale: ")
   LOG(inputScale)
   LOG(" --> ")
   LOG(normalized)
 
-  // correct the gamma
+  // correct the gamma for LED intensity and POV perception
   double output = convertGamma(normalized);
   LOG("; out: ")
   LOG(output)
@@ -76,9 +80,11 @@ void loop() {
 }
 
 double readNormalized() {
+  // get the time
   uint32_t nowMs = millis();
   uint32_t timeSinceLastSampleMs = nowMs - lastSampleTimeMs;
-  
+
+  // the input wave is centered around 0.55V
   // read the raw input and convert to magnitude from center
   int inValue = analogRead(PIN_IN);
   int zeroCentered = inValue - 512;
@@ -88,10 +94,11 @@ double readNormalized() {
   double normalized = absValue / 512.0;
   normalized = constrain(normalized, 0.0, 1.0);
 
-  // update running average
+  // update the low-pass filter with the new input value
   double sampleProportion = (double) timeSinceLastSampleMs / SAMPLE_WINDOW_MS;
   inputAccumulator = sampleProportion * normalized + (1.0 - sampleProportion) * inputAccumulator;
 
+  // remember the time
   lastSampleTimeMs = nowMs;
 
   return inputAccumulator;
@@ -104,14 +111,27 @@ double thresholdFilter(double input) {
   return input;
 }
 
+/* 
+ *  The eye is more sensitive to changes in intensity at low levels than at high
+ *  levels. Curve the output to counteract this property in order to approximate
+ *  a perceived linear intensity scale.
+ */
 double convertGamma(double x) {
-  return x * x * x;
+  return pow(x, 3.0);
 }
 
+/* 
+ *  Auto-leveling logic:
+ *  Record the maximum range of the input over a given window interval. Then, set the
+ *  scale so that this range is re-mapped onto the range [0..1]. Update the scale value
+ *  once every window interval, repeating continuously.  
+ */
 void adjustScaleForInput(double normalizedInput) {
+  // get the time
   uint32_t nowMs = millis();
   uint32_t timeSinceLastScaleUpdateMs = nowMs - lastScaleTimeMs;
 
+  // remember the maximum value
   autoLevelWindowMax = max(autoLevelWindowMax, normalizedInput);
 
   if (DEBUG) {
@@ -120,17 +140,20 @@ void adjustScaleForInput(double normalizedInput) {
     dtostrf(autoLevelWindowMax, 7, 5, str);
     LOG(str)
   }
-  
+
+  // if the window interval has elapsed, update the scale
   if (timeSinceLastScaleUpdateMs > AUTO_LEVEL_WINDOW_SIZE_MS) {
     inputScale = autoLevelWindowMax > 0 ? AUTO_LEVEL_WINDOW_MAX_TARGET / autoLevelWindowMax : DEFAULT_INPUT_SCALE;
 
+    // reset the max
     autoLevelWindowMax = 0;
-    
+
+    // reset the window timer
     lastScaleTimeMs = nowMs;
   }
 }
 
-// converts into an unsigned 32-bit int equal to the magnitude of the input
+// converts an int to an unsigned 32-bit int equal to the magnitude of the input
 uint32_t convertAbs(int val) {
   if (val < 0) {
     return (uint32_t) -val;
@@ -138,6 +161,7 @@ uint32_t convertAbs(int val) {
   return (uint32_t) val;
 }
 
+// sets the PWM outputs to have the given duty cycle
 void writeOutputPwm(double duty) {
   // map onto and constrain to [0..255]
   int output = (int) (duty * 255);
